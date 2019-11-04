2839fbb8bdf56122d4d5570c8bf498e0bb26b1b6
Jessica Rowland <rowlandjl@vcu.edu>
2019-11-04 02:44:16 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,4 +6,8 @@ class Song < ActiveRecord::Base
   def artist_name 
     self.artist ? self.artist.name : nil 
   end 
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by(name: name)
+  end 
 end
